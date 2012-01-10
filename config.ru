b354ac0305fdabca90310fcdda4421a7ecfc7c73
Bruno Aguirre <yo@brunoaguirre.com>
2012-01-09 22:00:39 -0200

    Initial idea

---
+++ config.ru
@@ -0,0 +1,1 @@
+run Rack::Static.new @app, urls: [""], root: 'test'
---
+++ test/ajax.js
@@ -0,0 +1,37 @@
+var scenario = require('gerbil').scenario;
+var window = { XMLHttpRequest: require('xmlhttprequest').XMLHttpRequest }
+var VCR = require('../lib/vcr');
+
+scenario("Ajax interception", {
+  'setup': function() {
+    VCR.configure(function(c) {
+      c.hookInto = window.XMLHttpRequest;
+    });
+  },
+
+  'Recording ajax request': function(g) {
+    VCR.useCassette('test', function(v) {
+      XMLHttpRequest = v.XMLHttpRequest;
+
+      var makeRequest = function() {
+        var ajax = new XMLHttpRequest();
+
+        ajax.open('GET', 'http://localhost:9292/test.html');
+        ajax.onreadystatechange = function() {
+          if(ajax.readyState === 4) {
+            g.assertEqual("Hello World!\n", ajax.responseText);
+          }
+        };
+        ajax.send(null);
+      }
+      // Record First Request
+      makeRequest();
+
+      // Wait for it...
+      g.setTimeout(function() {
+        makeRequest();
+        makeRequest();
+      }, 1000);
+    });
+ }
+});
