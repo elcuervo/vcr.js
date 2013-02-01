cecc47c309158db8f7fd9ad00f0a89c59dc9b053
Bruno Aguirre <yo@brunoaguirre.com>
2013-02-01 13:45:20 -0200

    Adds jquery support for recording

--- test/ajax.js
+++ test/ajax.js
@@ -1,23 +1,44 @@
-var scenario = require('gerbil').scenario;
-var window = { XMLHttpRequest: require('xmlhttprequest').XMLHttpRequest }
-var VCR = require('../lib/vcr');
+if(typeof module != 'undefined') {
+  var scenario = require('gerbil').scenario;
+  var $ = require('jquery');
+  var window = { XMLHttpRequest: require('xmlhttprequest').XMLHttpRequest }
+  var VCR = require('../lib/vcr');
+}
 
 scenario("Ajax interception", {
   'setup': function() {
     VCR.configure(function(c) {
       c.hookInto = window.XMLHttpRequest;
+      c.cassetteLibraryDir = "test/cassettes"
       c.host = "http://localhost:9292/"
     });
   },
 
-  'Recording ajax request': function(g) {
-    VCR.useCassette('test', function(v) {
+  'Recording a jquery ajax request': function(g) {
+    VCR.useCassette('jquery', function(v) {
+      XMLHttpRequest = v.XMLHttpRequest;
+
+      $.ajaxSettings.xhr = function(){
+        return new v.XMLHttpRequest();
+      }
+
+      var promise = $.get('test/test.html');
+
+      promise.then(function(response) {
+        g.assertEqual("Hello World!\n", response);
+      })
+
+    });
+  },
+
+  'Recording vanilla ajax request': function(g) {
+    VCR.useCassette('vanilla', function(v) {
       XMLHttpRequest = v.XMLHttpRequest;
 
       var makeRequest = function() {
         var ajax = new XMLHttpRequest();
 
-        ajax.open('GET', 'test.html');
+        ajax.open('GET', 'test/test.html');
         ajax.onreadystatechange = function() {
           if(ajax.readyState === 4) {
             g.assertEqual("Hello World!\n", ajax.responseText);
